7c82ca5cff82a55e9e0cef5a19b0c0ab85a26924
William J Teskey <willjteskey@gmail.com>
2018-04-13 21:54:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,5 +14,23 @@ def batch_badge_creator(attendees)
   list
 end
 
+def assign_rooms(attendees)
+  assignment = []
+  counter = 0
+  attendees.each do |attendee|
+    assignment.push("Hello, #{attendee}! You'll be assigned to room #{counter + 1}!")
+    counter += 1
+  end
+  assignment
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |attendee|
+    puts attendee
+  end
+  assign_rooms(attendees).each do |attendee|
+    puts attendee
+  end
+end
 
   
